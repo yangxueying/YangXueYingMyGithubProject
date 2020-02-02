da58f5b380f103431162bc4c033b20fa2de2630b
yangxueying <939968491@qq.com>
2020-02-02 13:16:51 +0800

    第二次从IDEA进行git提交到本地仓库

--- src/servlets/HelloServlet.java
+++ src/servlets/HelloServlet.java
@@ -12,7 +12,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
-        out.println("Hello Github Project!");
+        out.println("Hello My Github Project!");
 
     }
 }
